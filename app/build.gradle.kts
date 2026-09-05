d96a2094b9fffa9cca3394dd9b7db0cd9fa4ee98
megumiss <zlj12138@gmail.com>
2026-09-05 18:20:10 +0800

    android: 修复Termux脚本换行导致安装卡住

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.megumiss.nkas.mobile"
         minSdk = 30
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.3.3"
+        versionCode = 15
+        versionName = "0.3.4"
     }
 
     buildTypes {
