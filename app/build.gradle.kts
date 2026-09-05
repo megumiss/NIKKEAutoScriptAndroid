c712e2b7d215eac428286153a2423cb7b7b64c5c
megumiss <zlj12138@gmail.com>
2026-09-05 18:22:15 +0800

    android: 固定Termux脚本换行格式

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.megumiss.nkas.mobile"
         minSdk = 30
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.3.4"
+        versionCode = 16
+        versionName = "0.3.5"
     }
 
     buildTypes {
