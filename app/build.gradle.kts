7fbc69f01439e382bc49b1a79d968c09aabdd63b
megumiss <zlj12138@gmail.com>
2026-09-05 18:54:24 +0800

    android: 仅在Serial为auto时自动填充

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.megumiss.nkas.mobile"
         minSdk = 30
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.3.8"
+        versionCode = 20
+        versionName = "0.3.9"
     }
 
     buildTypes {
