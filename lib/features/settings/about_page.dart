39300308291cb797ae72918e0d03654246d2c17e
megumiss <zlj12138@gmail.com>
2026-09-16 07:22:49 +0800

    发布 1.2.5+4

--- lib/features/settings/about_page.dart
+++ lib/features/settings/about_page.dart
@@ -11,7 +11,7 @@ import 'package:nkas_mobile/core/widgets/surface.dart';
 import 'package:nkas_mobile/core/widgets/tag.dart';
 import 'package:nkas_mobile/theme.dart';
 
-const appVersion = '1.2.4';
+const appVersion = '1.2.5';
 
 const _projectRepoUrl = 'https://github.com/megumiss/NIKKEAutoScriptMobile';
 const _projectIssuesUrl = '$_projectRepoUrl/issues';
