bff2af3821b6f86c62b9855ca9cab13ca4cfbbb0
megumiss <zlj12138@gmail.com>
2026-09-16 18:23:08 +0800

    版本号提升至 1.2.7
    按补丁位递增用户可见版本，保留既有构建号 4。

--- lib/features/settings/about_page.dart
+++ lib/features/settings/about_page.dart
@@ -11,7 +11,7 @@ import 'package:nkas_mobile/core/widgets/surface.dart';
 import 'package:nkas_mobile/core/widgets/tag.dart';
 import 'package:nkas_mobile/theme.dart';
 
-const appVersion = '1.2.6';
+const appVersion = '1.2.7';
 
 const _projectRepoUrl = 'https://github.com/megumiss/NIKKEAutoScriptMobile';
 const _projectIssuesUrl = '$_projectRepoUrl/issues';
